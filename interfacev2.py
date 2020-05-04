f3f0610ac1abbc31cf292be1b8f73c934b818a2c
AdrienB4000 <60354760+AdrienB4000@users.noreply.github.com>
2020-05-04 09:23:27 +0200

    Add files via upload

--- interfacev2.py
+++ interfacev2.py
@@ -13,6 +13,7 @@ gamma_def=0.1
 S_def=0.9
 E_def=0.
 I_def=0.1
+R_def=0.
 Nb=1000000
 
 duree = 80 #jours
@@ -36,7 +37,6 @@ class Boutons:
         self.liste.insert(2, "Modèle SIR avec diffusion")
         self.liste.insert(3, "Modèle SEIR avec diffusion")
         self.liste.select_set(0)
-        a=self.liste.curselection()
 
         #Choix des paramètres d'évolution
         F=Frame(master)
@@ -59,18 +59,21 @@ class Boutons:
         #Choix des paramètres initiaux
         F2=LabelFrame(F, text="Paramètres initiaux")
 
-        self.S=Scale(F2, orient='horizontal', from_=0, to=1, resolution=0.01, label="Taux de susceptibles", tickinterval=0.25, length=150)
+        self.S=Scale(F2, orient='horizontal', from_=0, to=1, resolution=0.01, label="Taux de susceptibles", tickinterval=0.25, length=150, command=self.commande1)
         self.S.set(S_def)
         self.S.pack(side=TOP)
 
-        if ((a[0]==2) or (a[0]==3)):
-            self.E=Scale(F2, orient='horizontal', from_=0, to=1, resolution=0.01, label="Taux d'exposés", tickinterval=0.25, length=150)
-            self.E.set(E_def)
-            self.E.pack()
+        self.E=Scale(F2, orient='horizontal', from_=0, to=1, resolution=0.01, label="Taux d'exposés", tickinterval=0.25, length=150, command=self.commande1)
+        self.E.set(E_def)
+        self.E.pack()
 
-        self.I=Scale(F2, orient='horizontal', from_=0, to=1, resolution=0.01, label="Taux d'infectés", tickinterval=0.25, length=150)
+        self.I=Scale(F2, orient='horizontal', from_=0, to=1-self.S.get(), resolution=0.01, label="Taux d'infectés", tickinterval=0.25, length=150,command=self.commande1)
         self.I.set(I_def)
-        self.I.pack()
+        self.I.pack(side=BOTTOM)
+
+        self.R=Scale(F2, orient='horizontal', from_=0, to=1-self.S.get(), resolution=0.01, label="Taux de guéris", tickinterval=0.25, length=150,command=self.commande1)
+        self.R.set(R_def)
+        self.R.pack(side=BOTTOM)
 
         F2.pack(side=BOTTOM)
         F.pack(side=LEFT)
@@ -79,7 +82,7 @@ class Boutons:
         parametres=[Nb,self.beta.get(),self.gamma.get(),0,0,0,self.alpha.get(),0,0]
         methode=prop.Runge_Kutta_4
         modele=liste_modeles[(self.liste.curselection())[0]]
-        u0=[0,Nb*self.S.get(),0,Nb*self.I.get(),Nb*(1-self.S.get()-self.I.get())]
+        u0=[0,Nb*self.S.get(),Nb*self.E.get(),Nb*self.I.get(),Nb*self.R.get()]
         print(self.beta.get())
         cree=True
 
@@ -91,11 +94,12 @@ class Boutons:
         Recovered=U[:,4]
 
         fig = Figure()
-        a = fig.add_subplot(111)
-        self.p1,=a.plot(temps,Susceptible,':',color="blue",label="S")
-        self.p2,=a.plot(temps,Infected,':',color="red",label="I")
-        self.p3,=a.plot(temps,Recovered,':',color="green",label="R")
-        self.p4,=a.plot(temps,Exposed,':',color="orange",label="R")
+        self.a = fig.add_subplot(111)
+        self.p1,=self.a.plot(temps,Susceptible,':',color="blue",label="S")
+        self.p2,=self.a.plot(temps,Infected,':',color="red",label="I")
+        self.p3,=self.a.plot(temps,Recovered,':',color="green",label="R")
+        self.p4,=self.a.plot(temps,Exposed,':',color="orange",label="E")
+        self.a.legend()
         self.canvas = FigureCanvasTkAgg(fig,master=master)
         self.canvas.get_tk_widget().pack()
 
@@ -103,12 +107,28 @@ class Boutons:
         b=Button(master, text="Actualiser", command=self.affiche)
         b.pack(side=BOTTOM)
 
+    def change_limites_E(self,val):
+        self.S.configure(to=1-self.E.get()-self.I.get()-self.R.get(), tickinterval=(1-self.E.get()-self.I.get()-self.R.get())/4)
+        self.E.configure(to=1-self.S.get()-self.I.get()-self.R.get(),tickinterval=(1-self.S.get()-self.I.get()-self.R.get())/4)
+        self.I.configure(to=1-self.E.get()-self.S.get()-self.R.get(),tickinterval=(1-self.E.get()-self.S.get()-self.R.get())/4)
+        self.R.configure(to=1-self.E.get()-self.I.get()-self.S.get(),tickinterval=(1-self.E.get()-self.I.get()-self.S.get())/4)
+
+    def change_limites_noE(self,val):
+        self.S.configure(to=1-self.I.get()-self.R.get(), tickinterval=(1-self.I.get()-self.R.get())/4)
+        self.I.configure(to=1-self.S.get()-self.R.get(),tickinterval=(1-self.S.get()-self.R.get())/4)
+        self.R.configure(to=1-self.I.get()-self.S.get(),tickinterval=(1-self.I.get()-self.S.get())/4)
+
+    def commande1(self,val):
+        if ((self.liste.curselection())[0]==1 or (self.liste.curselection())[0]==3):
+            self.change_limites_E(val)
+        else:
+            self.change_limites_noE(val)
+
     def affiche(self):
         parametres=[Nb,self.beta.get(),self.gamma.get(),0,0,0,self.alpha.get(),0,0]
         methode=prop.Runge_Kutta_4
         modele=liste_modeles[(self.liste.curselection())[0]]
-        u0=[0,Nb*self.S.get(),0,Nb*self.I.get(),Nb*(1-self.S.get()-self.I.get())]
-        print(self.beta.get())
+        u0=[0,Nb*self.S.get(),Nb*self.E.get(),Nb*self.I.get(),Nb*self.R.get()]
         cree=True
 
         U=prop.methode(u0,modele,parametres)
@@ -122,12 +142,13 @@ class Boutons:
         self.p2.set_ydata(Infected)
         self.p3.set_ydata(Recovered)
         self.p4.set_ydata(Exposed)
+        self.a.legend()
         self.canvas.draw()
 
 #Ouverture de la fenetre
 fenetre = Tk()
 fenetre.title("Modélisation de la propagation d'une épidémie")
-fenetre.geometry("1300x600")
+fenetre.geometry("1300x600+30+30")
 
 boutons=Boutons(fenetre)
 fenetre.mainloop()
