9be5655d2a5fe937208a0e537419962bd3233c86
antho-stz <63554227+antho-stz@users.noreply.github.com>
2020-05-27 18:39:38 +0200

    Update methodes_resolution.py

--- Propagation d'une epidemie/methodes_resolution.py
+++ Propagation d'une epidemie/methodes_resolution.py
@@ -28,25 +28,6 @@ def Euler_explicite(u0,f,parametres,temps,pas_t):
         U.append(u)
     return np.array(U)
 
-def Newton_lent(x0,h):
-    """Resout l'equation h(x)=0 en partant de x0 avec la methode de Newton."""
-    x = x0
-    epsilon = 1e-5
-    nb_max = 100
-    delta = 1e-7
-    for i in range(nb_max):
-        dh = np.array([(h([x[j]+delta*(j==i) for j in range(len(x))])-h(x))/delta for i in range(len(x))])
-        norm_dh2 = np.linalg.norm(dh)**2
-        if norm_dh2<epsilon:
-            print("Probleme : impossible de resoudre le modele avec la methode d'Euler implicite : division par 0 lors de la methode de Newton !\n")
-            break
-        else:
-            dx = -h(x)*dh/norm_dh2
-            x = x + dx
-            if np.linalg.norm(dx) < epsilon:
-                break
-    return x
-
 def Newton(x0,h,jacobienne,pas_t,parametres):
     """Resout l'equation h(x)=0 en partant de x0 avec la methode de Newton."""
     x = x0
@@ -69,6 +50,7 @@ def Euler_implicite(u0,f,parametres,temps,pas_t):
     """Resout du/dt=f(u) avec la methode d'Euler implicite."""
     U = [u0]
     u = u0
+
     # Calcul de la matrice A, necessaire uniquement pour la diffusion
     nb_pts_x = len(u0)
     pas_x = 1 / nb_pts_x # car la distance est unitaire
@@ -83,20 +65,23 @@ def Euler_implicite(u0,f,parametres,temps,pas_t):
     A/=pas_x**2
 
     # Résolution
+    # On change la forme de u pour pouvoir utiliser Newton, qui ne s'utilise qu'avec un vecteur
     def h(x):
-        return x-u-pas_t*f(x,parametres,A)
-    def jac_h(x):
-        return np.identity(5) - pas_t*mod.jacobiennes[mod.modeles.index(f)](x,parametres)
+        return x-u-pas_t*np.reshape(f(np.reshape(x,np.shape(u0)),parametres,A),-1)
+    def jac_h(h,x):
+        eps = 1e-6
+        return np.identity(len(x)) - pas_t*mod.jacobiennes[mod.modeles.index(f)](x,parametres)
+    u = np.reshape(u,-1)
     for t in temps[:-1]:
-        #u = Newton_lent(u,lambda x : np.linalg.norm(x-u-pas_t*f(x,parametres,A)))
         u = Newton(u,h,jac_h,pas_t,parametres)
-        U.append(u)
+        U.append(np.reshape(u,np.shape(u0)))
     return np.array(U)
 
 def Heun(u0,f,parametres,temps,pas_t):
     """Resout du/dt=f(u) avec la methode de Heun."""
     U = [u0]
     u = u0
+
     # Calcul de la matrice A, necessaire uniquement pour la diffusion
     nb_pts_x = len(u0)
     pas_x = 1 / nb_pts_x # car la distance est unitaire
@@ -120,6 +105,7 @@ def Runge_Kutta_2(u0,f,parametres,temps,pas_t):
     """Resout du/dt=f(u) avec la methode de Runge-Kutta d'ordre 2."""
     U = [u0]
     u = u0
+
     # Calcul de la matrice A, necessaire uniquement pour la diffusion
     nb_pts_x = len(u0)
     pas_x = 1 / nb_pts_x # car la distance est unitaire
@@ -143,6 +129,7 @@ def Runge_Kutta_4(u0,f,parametres,temps,pas_t):
     """Resout du/dt=f(u) avec la methode de Runge-Kutta d'ordre 4."""
     U = [u0]
     u = u0
+
     # Calcul de la matrice A, necessaire uniquement pour la diffusion
     nb_pts_x = len(u0)
     pas_x = 1 / nb_pts_x # car la distance est unitaire
